0100007e99f27609e0c165eb98be2aa7464c7725
Satoshi Konno <skonno@cybergarage.org>
2022-11-19 00:56:17 +0900

    Add error constants

---
+++ safecast/errors.go
@@ -0,0 +1,40 @@
+// Copyright (C) 2022 The go-safecast Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package safecast
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrCast = errors.New("cast error")
+
+const (
+	errorCastType  = "%w : %v (%T) => %T"
+	errorOverflow  = "%w : overflow %v (%T)  => %T"
+	errorUnderflow = "%w : underflow %v (%T) => %T"
+)
+
+func newErrorCast(fromItem any, toItem any) error {
+	return fmt.Errorf(errorCastType, ErrCast, fromItem, fromItem, toItem)
+}
+
+func newErrorOverflow(fromItem any, toItem any) error {
+	return fmt.Errorf(errorOverflow, ErrCast, fromItem, fromItem, toItem)
+}
+
+func newErrorUnderflow(fromItem any, toItem any) error {
+	return fmt.Errorf(errorUnderflow, ErrCast, fromItem, fromItem, toItem)
+}
