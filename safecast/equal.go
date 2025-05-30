fc22f2a46e1215cb2f3e3d6268b046c38784abb5
Satoshi Konno <skonno@cybergarage.org>
2025-05-30 20:42:27 +0900

    Fix Equal(): handle nil pointer comparisons

--- safecast/equal.go
+++ safecast/equal.go
@@ -26,118 +26,164 @@ import (
 func Equal(v1 any, v2 any) bool {
 	eq := func(v1, v2 any) bool {
 
-		eqInt := func(v1 int, v2 any) bool {
+		eqInt := func(v1 *int, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 int
 			if err := ToInt(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqInt8 := func(v1 int8, v2 any) bool {
+		eqInt8 := func(v1 *int8, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 int8
 			if err := ToInt8(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqInt16 := func(v1 int16, v2 any) bool {
+		eqInt16 := func(v1 *int16, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 int16
 			if err := ToInt16(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqInt32 := func(v1 int32, v2 any) bool {
+		eqInt32 := func(v1 *int32, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 int32
 			if err := ToInt32(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqInt64 := func(v1 int64, v2 any) bool {
+		eqInt64 := func(v1 *int64, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 int64
 			if err := ToInt64(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqUint := func(v1 uint, v2 any) bool {
+		eqUint := func(v1 *uint, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 uint
 			if err := ToUint(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
-		eqUint8 := func(v1 uint8, v2 any) bool {
+
+		eqUint8 := func(v1 *uint8, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 uint8
 			if err := ToUint8(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqUint16 := func(v1 uint16, v2 any) bool {
+		eqUint16 := func(v1 *uint16, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 uint16
 			if err := ToUint16(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqUint32 := func(v1 uint32, v2 any) bool {
+		eqUint32 := func(v1 *uint32, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 uint32
 			if err := ToUint32(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqUint64 := func(v1 uint64, v2 any) bool {
+		eqUint64 := func(v1 *uint64, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 uint64
 			if err := ToUint64(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqFloat32 := func(v1 float32, v2 any) bool {
+		eqFloat32 := func(v1 *float32, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 float32
 			if err := ToFloat32(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqFloat64 := func(v1 float64, v2 any) bool {
+		eqFloat64 := func(v1 *float64, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 float64
 			if err := ToFloat64(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqBool := func(v1 bool, v2 any) bool {
+		eqBool := func(v1 *bool, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 bool
 			if err := ToBool(v2, &cv2); err != nil {
 				return false
 			}
-			return cv2 == v1
+			return cv2 == *v1
 		}
 
-		eqString := func(v1 string, v2 any) bool {
+		eqString := func(v1 *string, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 string
 			if err := ToString(v2, &cv2); err != nil {
 				return false
 			}
-			return strings.EqualFold(cv2, v1)
+			return strings.EqualFold(cv2, *v1)
 		}
 
 		eqBytes := func(v1 []byte, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 []byte
 			if err := ToBytes(v2, &cv2); err != nil {
 				return false
@@ -145,7 +191,10 @@ func Equal(v1 any, v2 any) bool {
 			return bytes.Equal(cv2, v1)
 		}
 
-		eqTime := func(v1 time.Time, v2 any) bool {
+		eqTime := func(v1 *time.Time, v2 any) bool {
+			if v1 == nil {
+				return v2 == nil
+			}
 			var cv2 time.Time
 			if err := ToTime(v2, &cv2); err != nil {
 				return false
@@ -155,115 +204,67 @@ func Equal(v1 any, v2 any) bool {
 
 		switch v1 := v1.(type) {
 		case int:
-			return eqInt(v1, v2)
+			return eqInt(&v1, v2)
 		case *int:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqInt(*v1, v2)
+			return eqInt(v1, v2)
 		case int8:
-			return eqInt8(v1, v2)
+			return eqInt8(&v1, v2)
 		case *int8:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqInt8(*v1, v2)
+			return eqInt8(v1, v2)
 		case int16:
-			return eqInt16(v1, v2)
+			return eqInt16(&v1, v2)
 		case *int16:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqInt16(*v1, v2)
+			return eqInt16(v1, v2)
 		case int32:
-			return eqInt32(v1, v2)
+			return eqInt32(&v1, v2)
 		case *int32:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqInt32(*v1, v2)
+			return eqInt32(v1, v2)
 		case int64:
-			return eqInt64(v1, v2)
+			return eqInt64(&v1, v2)
 		case *int64:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqInt64(*v1, v2)
+			return eqInt64(v1, v2)
 		case uint:
-			return eqUint(v1, v2)
+			return eqUint(&v1, v2)
 		case *uint:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqUint(*v1, v2)
+			return eqUint(v1, v2)
 		case uint8:
-			return eqUint8(v1, v2)
+			return eqUint8(&v1, v2)
 		case *uint8:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqUint8(*v1, v2)
+			return eqUint8(v1, v2)
 		case uint16:
-			return eqUint16(v1, v2)
+			return eqUint16(&v1, v2)
 		case *uint16:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqUint16(*v1, v2)
+			return eqUint16(v1, v2)
 		case uint32:
-			return eqUint32(v1, v2)
+			return eqUint32(&v1, v2)
 		case *uint32:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqUint32(*v1, v2)
+			return eqUint32(v1, v2)
 		case uint64:
-			return eqUint64(v1, v2)
+			return eqUint64(&v1, v2)
 		case *uint64:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqUint64(*v1, v2)
+			return eqUint64(v1, v2)
 		case float32:
-			return eqFloat32(v1, v2)
+			return eqFloat32(&v1, v2)
 		case *float32:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqFloat32(*v1, v2)
+			return eqFloat32(v1, v2)
 		case float64:
-			return eqFloat64(v1, v2)
+			return eqFloat64(&v1, v2)
 		case *float64:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqFloat64(*v1, v2)
+			return eqFloat64(v1, v2)
 		case bool:
-			return eqBool(v1, v2)
+			return eqBool(&v1, v2)
 		case *bool:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqBool(*v1, v2)
+			return eqBool(v1, v2)
 		case string:
-			return eqString(v1, v2)
+			return eqString(&v1, v2)
 		case *string:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqString(*v1, v2)
+			return eqString(v1, v2)
 		case []byte:
-			if v1 == nil {
-				return v2 == nil
-			}
 			return eqBytes(v1, v2)
 		case time.Time:
-			return eqTime(v1, v2)
+			return eqTime(&v1, v2)
 		case *time.Time:
-			if v1 == nil {
-				return v2 == nil
-			}
-			return eqTime(*v1, v2)
+			return eqTime(v1, v2)
 		case nil:
 			if v2 == nil {
 				return true
