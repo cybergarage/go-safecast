bc085fedecb983dcdebeb8e148f04952a7adfa72
Satoshi Konno <skonno@cybergarage.org>
2025-11-23 13:01:38 +0900

    feat: add map comparison support to Equal()

--- safecast/equal.go
+++ safecast/equal.go
@@ -24,22 +24,65 @@ func Equal(v1 any, v2 any) bool {
 	if reflect.DeepEqual(v1, v2) {
 		return true
 	}
-	switch v1 := v1.(type) {
-	case []any:
-		switch v2.(type) {
-		case []any:
-			if len(v1) != len(v2.([]any)) {
-				return false
-			}
-			for i := range v1 {
-				if !Equal(v1[i], v2.([]any)[i]) {
-					return false
-				}
-			}
-			return true
-		default:
+
+	equalSlices := func(v1, v2 []any) bool {
+		if len(v1) != len(v2) {
 			return false
 		}
+		for i := range v1 {
+			if !Equal(v1[i], v2[i]) {
+				return false
+			}
+		}
+		return true
+	}
+
+	equalMaps := func(v1Map, v2Map map[any]any) bool {
+		if len(v1Map) != len(v2Map) {
+			return false
+		}
+		for key, v1Val := range v1Map {
+			v2Val, ok := v2Map[key]
+			if !ok {
+				return false
+			}
+			if !Equal(v1Val, v2Val) {
+				return false
+			}
+		}
+		return true
+	}
+
+	convertMapStringToAny := func(m map[string]any) map[any]any {
+		result := make(map[any]any, len(m))
+		for k, v := range m {
+			result[k] = v
+		}
+		return result
+	}
+
+	switch v1 := v1.(type) {
+	case []any:
+		if v2Slice, ok := v2.([]any); ok {
+			return equalSlices(v1, v2Slice)
+		}
+		return false
+	case map[string]any:
+		switch v2 := v2.(type) {
+		case map[string]any:
+			return equalMaps(convertMapStringToAny(v1), convertMapStringToAny(v2))
+		case map[any]any:
+			return equalMaps(convertMapStringToAny(v1), v2)
+		}
+		return false
+	case map[any]any:
+		switch v2 := v2.(type) {
+		case map[any]any:
+			return equalMaps(v1, v2)
+		case map[string]any:
+			return equalMaps(v1, convertMapStringToAny(v2))
+		}
+		return false
 	}
 
 	cmp, err := Compare(v1, v2)
